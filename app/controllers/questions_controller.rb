77e201a788345936613394566c33e61e8f72a145
mathieu hollande <mathieuhollande@hotmail.fr>
2021-11-15 15:13:52 +0100

    success

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,14 @@ class QuestionsController < ApplicationController
   def ask
 
   end
-end
+
+  def answer
+    if params[:name] == 'i am going to work right now!'
+    @answer = 'great'
+    elsif params[:name].include?('?')
+    @answer = 'Silly question, get dressed and go to work!'
+    elsif params[:name].is_a? String
+    @answer = "I don't care, get dressed and go to work!"
+    end
+   end
+  end
